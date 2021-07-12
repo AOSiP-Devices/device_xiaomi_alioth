56dada06f5383d09b0b996956ac3b7fc6865111e
GtrCraft <nickvanbokhorst@hotmail.com>
2021-07-12 16:25:38 +0200

    alioth: AICP bringup
    Change-Id: I1cc019003c90f5bac509956b25d26159821a6477

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/aicp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    aicp_alioth-user \
+    aicp_alioth-userdebug \
+    aicp_alioth-eng
--- aicp_alioth.mk
+++ aicp_alioth.mk
@@ -8,13 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aicp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
